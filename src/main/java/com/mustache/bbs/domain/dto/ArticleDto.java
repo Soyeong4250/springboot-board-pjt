a67364ef74bd9c4c4ab7b138d6bfff02eee7f67d
이소영 <th42500@naver.com>
2022-11-09 12:49:01 +0900

    [Feat] ArticleDto 구현
    1. @Getter 선언
    2. 생성자 선언
    3. ArticleDto를 Article Entity 객체로 바꾸는 toEntity() 추가

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,20 @@
+package com.mustache.bbs.domain.dto;
+
+import com.mustache.bbs.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleDto {
+
+    private String title;
+    private String content;
+
+    public ArticleDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article toEntity() {
+        return new Article(this.title, this.content);
+    }
+}
