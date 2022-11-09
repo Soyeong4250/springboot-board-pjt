0ff46139e57055e731ee6b36610ec945721eaf20
이소영 <th42500@naver.com>
2022-11-09 13:08:18 +0900

    [Feat] selectArticle() 추가
    게시글 상세보기 기능 구현

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -23,6 +27,18 @@ public class ArticleController {
     @GetMapping("/new")
     public String registerPage(){
         return "articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String selectArticle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if(!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "error";
+        }
     }
 
     @PostMapping("")
