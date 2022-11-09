4aedd179bac2c2ad2eb50630226945c22028dfb8
이소영 <th42500@naver.com>
2022-11-09 12:46:02 +0900

    [Feat] Article Entity 객체 생성
    @Entity, @Table, @NoArgsConstructor, @Id, @GeneratedValue 추가

---
+++ src/main/java/com/mustache/bbs/domain/Article.java
@@ -0,0 +1,25 @@
+package com.mustache.bbs.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "article")
+@NoArgsConstructor
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
