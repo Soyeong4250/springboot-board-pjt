de2c72441d5a6318e6d37ce8bd87218a4b602314
이소영 <th42500@naver.com>
2022-11-09 12:55:06 +0900

    [Feat] 게시글 등록 기능 추가
    logger를 이용한 ArticleDto 확인

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.mustache.bbs.controller;
 
+import com.mustache.bbs.domain.dto.ArticleDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +16,11 @@ public class ArticleController {
     public String registerPage(){
         return "articles/new";
     }
+
+    @PostMapping("")
+    public String articles(ArticleDto articleDto) {
+        log.info(articleDto.toString());
+        return "";
+    }
+
 }
