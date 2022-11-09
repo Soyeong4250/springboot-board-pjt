d04dd8b98d9d9646df8363be9162d3814fcaf48f
이소영 <th42500@naver.com>
2022-11-09 12:35:14 +0900

    [Feat] ArticleCotroller - registerPage() 추가
    게시글 등록 페이지 띄우기 메서드 구현

---
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -0,0 +1,17 @@
+package com.mustache.bbs.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/articles")
+public class ArticleController {
+
+    @GetMapping("/new")
+    public String registerPage(){
+        return "articles/new";
+    }
+}
